7f8bcfb544027720d84d43e3f295972d9a077621
bytedream <bytedream@protonmail.com>
2022-08-03 19:53:14 +0200

    Add test session caching

--- tests/utils/session.rs
+++ tests/utils/session.rs
@@ -1,9 +1,18 @@
 use std::error::Error;
+use once_cell::sync::OnceCell;
 use crunchyroll_rs::Crunchyroll;
 use crate::utils::store::{get_store, has_store, set_store};
 
+const SESSION: OnceCell<Crunchyroll> = OnceCell::new();
+
 pub async fn get_session() -> Result<Crunchyroll, Box<dyn Error>> {
-    Ok(Crunchyroll::new().login_with_etp_rt(get_store("session".into())?).await?)
+    if let Some(session) = SESSION.get() {
+        Ok(session.clone())
+    } else {
+        let session = Crunchyroll::new().login_with_etp_rt(get_store("session".into())?).await?;
+        SESSION.set(session.clone()).unwrap();
+        Ok(session)
+    }
 }
 
 pub fn set_session(crunchy: Crunchyroll) -> Result<(), Box<dyn Error>> {
