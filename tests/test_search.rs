6ecd523e1469c45cab86ac90c04c561573ac00ed
bytedream <bytedream@protonmail.com>
2022-08-27 20:22:36 +0200

    Add more query search tests

--- tests/test_search.rs
+++ tests/test_search.rs
@@ -1,3 +1,4 @@
+use crunchyroll_rs::search::query::{QueryOptions, QueryType};
 use crate::utils::SESSION;
 
 mod utils;
@@ -6,7 +7,22 @@ mod utils;
 async fn by_query() {
     let crunchy = SESSION.get().await.unwrap();
 
-    let result = crunchy.query("darling".into(), Default::default()).await;
+    let default_result = crunchy.query("darling".into(), Default::default()).await;
+    assert!(default_result.is_ok(), "{}", default_result.unwrap_err().to_string());
 
-    assert!(result.is_ok(), "{}", result.unwrap_err().to_string())
+    let zero_limit_result = crunchy.query("test".into(), QueryOptions::default().limit(0)).await;
+    assert!(zero_limit_result.is_ok(), "{}", default_result.unwrap_err().to_string());
+    let zero_limit_result_unwrapped = zero_limit_result.unwrap();
+    assert!(zero_limit_result_unwrapped.top_results.is_none(), "'top_results' is not None");
+    assert!(zero_limit_result_unwrapped.series.is_none(), "'series' is not None");
+    assert!(zero_limit_result_unwrapped.movie_listing.is_none(), "'movie_listing' is not None");
+    assert!(zero_limit_result_unwrapped.episode.is_none(), "'episode' is not None");
+
+    let series_result = crunchy.query("test".into(), QueryOptions::default().result_type(Some(QueryType::Series))).await;
+    assert!(series_result.is_ok(), "{}", default_result.unwrap_err().to_string());
+    let series_result_unwrapped = series_result.unwrap();
+    assert!(series_result_unwrapped.top_results.is_none(), "'top_results' is not None");
+    assert!(series_result_unwrapped.series.is_some(), "'series' is not Some");
+    assert!(series_result_unwrapped.movie_listing.is_none(), "'movie_listing' is not None");
+    assert!(series_result_unwrapped.episode.is_none(), "'episode' is not None");
 }
