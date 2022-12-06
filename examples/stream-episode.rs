0212ded8a193552e1df409f7160dfdef555f025f
bytedream <bytedream@protonmail.com>
2022-12-06 09:32:35 +0100

    Fix stream example

--- examples/stream-episode.rs
+++ examples/stream-episode.rs
@@ -1,4 +1,4 @@
-#![cfg(feature = "stream")]
+#![cfg(feature = "hls-stream")]
 
 use anyhow::Result;
 use crunchyroll_rs::{Crunchyroll, Episode, Media};
@@ -15,7 +15,7 @@ async fn main() -> Result<()> {
 
     let episode: Media<Episode> = crunchyroll.media_from_id("GRDKJZ81Y").await?;
     let streams = episode.streams().await?;
-    let mut default_streams = streams.streaming_data(None).await?;
+    let mut default_streams = streams.hls_streaming_data(None).await?;
     // sort after resolutions; best to worst
     default_streams.sort_by(|a, b| a.resolution.width.cmp(&b.resolution.width).reverse());
 
