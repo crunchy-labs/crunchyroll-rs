9dfb8a4ba9a4d62b3f8006c36839a06cd4410d45
bytedream <bytedream@protonmail.com>
2023-07-15 22:02:30 +0200

    Update user agent

--- src/crunchyroll.rs
+++ src/crunchyroll.rs
@@ -485,7 +485,7 @@ mod auth {
             Client::builder()
                 .https_only(true)
                 .cookie_store(true)
-                .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.46a")
+                .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.67a")
                 .use_preconfigured_tls(tls_config)
         }
 
