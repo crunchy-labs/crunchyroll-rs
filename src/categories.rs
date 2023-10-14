be1ec75000ce614de7ecda75a70f2a4bf390f360
bytedream <bytedream@protonmail.com>
2023-10-14 14:04:01 +0200

    Add function to get the sub-categories of a category

--- src/categories.rs
+++ src/categories.rs
@@ -1,9 +1,11 @@
 //! Media categories.
 
 use crate::common::{Image, V2BulkResult};
+use crate::crunchyroll::Executor;
 use crate::Result;
 use crate::{enum_values, Crunchyroll, Locale, Request};
 use serde::{Deserialize, Serialize};
+use std::sync::Arc;
 
 enum_values! {
     /// Video categories / genres.
@@ -23,13 +25,29 @@ enum_values! {
         Sports = "sports"
         Supernatural = "supernatural"
         Thriller = "thriller"
-        Historical = "historical"
-        Isekai = "isekai"
-        Mystery = "mystery"
-        Idols = "idols"
+        // the following categories are sub-categories, they're not listed when calling
+        // `Crunchyroll::categories`
         Harem = "harem"
+        Historical = "historical"
+        Idols = "idols"
+        Isekai = "isekai"
         Mecha = "mecha"
+        Mystery = "mystery"
         PostApocalyptic = "post-apocalyptic"
+    }
+}
+
+impl Category {
+    pub fn sub_categories() -> Vec<Category> {
+        vec![
+            Category::Harem,
+            Category::Historical,
+            Category::Idols,
+            Category::Isekai,
+            Category::Mecha,
+            Category::Mystery,
+            Category::PostApocalyptic,
+        ]
     }
 }
 
@@ -58,11 +76,14 @@ pub struct CategoryInformationLocalization {
     pub locale: Locale,
 }
 
-/// A anime category / genre.
+/// An anime category / genre.
 #[derive(Clone, Debug, Default, Deserialize, Serialize, Request)]
 #[cfg_attr(feature = "__test_strict", serde(deny_unknown_fields))]
 #[cfg_attr(not(feature = "__test_strict"), serde(default))]
 pub struct CategoryInformation {
+    #[serde(skip)]
+    executor: Arc<Executor>,
+
     #[serde(rename = "id")]
     pub category: Category,
     pub slug: String,
@@ -73,8 +94,43 @@ pub struct CategoryInformation {
     pub localization: CategoryInformationLocalization,
 }
 
+impl CategoryInformation {
+    /// Get all sub-categories of this category.
+    pub async fn sub_categories(&self) -> Result<Vec<SubCategoryInformation>> {
+        let endpoint = format!(
+            "https://www.crunchyroll.com/content/v2/discover/categories/{}/sub_categories",
+            self.category
+        );
+        Ok(self
+            .executor
+            .get(endpoint)
+            .apply_locale_query()
+            .request::<V2BulkResult<SubCategoryInformation>>()
+            .await?
+            .data)
+    }
+}
+
+/// A sub category of an anime category / genre.
+#[derive(Clone, Debug, Default, Deserialize, Serialize, Request)]
+#[cfg_attr(feature = "__test_strict", serde(deny_unknown_fields))]
+#[cfg_attr(not(feature = "__test_strict"), serde(default))]
+pub struct SubCategoryInformation {
+    #[serde(rename = "id")]
+    pub category: Category,
+    #[serde(rename = "parent_category_id")]
+    pub parent_category: Category,
+
+    pub slug: String,
+
+    /// A human readable title & description about the category.
+    pub localization: CategoryInformationLocalization,
+}
+
 impl Crunchyroll {
-    /// Returns all video categories.
+    /// Returns all video categories. Note that not all categories declared in [`Category`] are
+    /// returned since some of them are sub-categories. Call [`Category::sub_categories`] to get a
+    /// list of categories which are sub-categories.
     pub async fn categories(&self) -> Result<Vec<CategoryInformation>> {
         let endpoint = "https://www.crunchyroll.com/content/v2/discover/categories";
         Ok(self
--- tests/test_categories.rs
+++ tests/test_categories.rs
@@ -7,3 +7,10 @@ async fn categories() {
     let crunchy = SESSION.get().await.unwrap();
     assert_result!(crunchy.categories().await)
 }
+
+#[tokio::test]
+async fn sub_categories() {
+    let crunchy = SESSION.get().await.unwrap();
+    let categories = crunchy.categories().await.unwrap();
+    assert_result!(categories.first().unwrap().sub_categories().await)
+}
