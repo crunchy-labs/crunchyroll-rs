9e444bb5a25658b0b85fb7073718f4c3620f540d
bytedream <bytedream@protonmail.com>
2024-04-21 16:39:59 +0200

    Simplify stream episode example

--- examples/stream-episode.rs
+++ examples/stream-episode.rs
@@ -1,8 +1,7 @@
-#![cfg(feature = "hls-stream")]
-
 use anyhow::Result;
 use crunchyroll_rs::{Crunchyroll, Episode};
 use std::env;
+use std::io::Write;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -14,18 +13,24 @@ async fn main() -> Result<()> {
         .await?;
 
     let episode: Episode = crunchyroll.media_from_id("GRDKJZ81Y").await?;
-    let streams = episode.stream().await?;
-    let mut default_streams = streams.hls_streaming_data(None).await?;
+    let stream = episode.stream_maybe_without_drm().await?;
+    let (mut video_streams, _audio_streams) = stream.stream_data(None).await?.unwrap();
     // sort after resolutions; best to worst
-    default_streams.sort_by(|a, b| a.resolution.width.cmp(&b.resolution.width).reverse());
+    video_streams.sort_by(|a, b| {
+        a.resolution()
+            .unwrap()
+            .width
+            .cmp(&b.resolution().unwrap().width)
+            .reverse()
+    });
 
     // get video segments of the stream with the best available resolution
-    let segments = default_streams[0].segments().await?;
+    let segments = video_streams[0].segments();
 
     let sink = &mut std::io::sink();
     for (i, segment) in segments.iter().enumerate() {
-        println!("Writing segment {} of {}", i + 1, segments.len() + 1);
-        segment.clone().write_to(sink).await?;
+        println!("Downloading segment {} of {}", i + 1, segments.len() + 1);
+        sink.write_all(&segment.data().await?)?;
     }
 
     Ok(())
