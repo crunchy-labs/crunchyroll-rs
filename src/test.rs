55d9e52e1aa5b005b6837905fada908c8fcd2cb8
bytedream <me@bytedream.dev>
2025-11-13 13:28:50 +0100

    Hide internal test methods from docs

--- src/test.rs
+++ src/test.rs
@@ -1,9 +1,10 @@
-//! Internal functions for integration tests.
+//! Internals for integration tests.
 
 use crate::{Crunchyroll, Result};
 use chrono::Utc;
 
 impl Crunchyroll {
+    #[doc(hidden)]
     pub async fn refresh_jwt(&self) -> Result<()> {
         self.executor.session.write().await.session_expire = Utc::now();
         let _ = self
