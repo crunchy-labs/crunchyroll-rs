49301518bd6794fb7bdb760dd97c626539255b3c
bytedream <me@bytedream.dev>
2025-11-14 19:16:56 +0100

    Skip rustfmt for basic auth token and user agent

--- src/crunchyroll.rs
+++ src/crunchyroll.rs
@@ -744,8 +744,9 @@ mod auth {
     }
 
     impl CrunchyrollBuilder {
-        pub const BASIC_AUTH_TOKEN: &'static str =
-            "bGtlc2k3c25zeTlvb2ptaTJyOWg6LWFHRFhGRk5UbHVaTUxZWEVSbmdOWW5FanZnSDVvZHY=";
+        #[rustfmt::skip] // for scripts that may fetch this
+        pub const BASIC_AUTH_TOKEN: &'static str = "bGtlc2k3c25zeTlvb2ptaTJyOWg6LWFHRFhGRk5UbHVaTUxZWEVSbmdOWW5FanZnSDVvZHY=";
+        #[rustfmt::skip] // for scripts that may fetch this
         pub const USER_AGENT: &'static str = "Crunchyroll/ANDROIDTV/3.49.1_22281 (Android 13.0; en-US; TCL-S5400AF Build/TP1A.220624.014)";
 
         pub const DEFAULT_HEADERS: [(HeaderName, HeaderValue); 4] = [
