0c65b77821455ef3a5b5e17e44dc04de84811613
bytedream <63594396+bytedream@users.noreply.github.com>
2026-09-15 04:17:16 +0000

    [update-credentials action] Update auth credentials & user-agent

--- src/crunchyroll.rs
+++ src/crunchyroll.rs
@@ -829,9 +829,9 @@ mod auth {
         /// up-to-date by this crate but may still become invalid between releases. If logins
         /// start to fail, you may need to supply a fresh token via [`CrunchyrollBuilder::platform`].
         #[rustfmt::skip] // for scripts that may fetch this
-        pub const ANDROID_TV_BASIC_AUTH_TOKEN: &'static str = "ZXZ4YzVybGN1bnd4cm91YWpmeHI6NkJGWGM1SUk3UWx2Z3NFbzdiVjBuWUNfN1VRLXVlSVM=";
+        pub const ANDROID_TV_BASIC_AUTH_TOKEN: &'static str = "dng1bThwOWZzYm16em15bnZnemo6Q2NjNXp6UkNxTDgtNWpVTDdJZFJRODU2Z0l6WlVZVk0=";
         #[rustfmt::skip] // for scripts that may fetch this
-        pub const ANDROID_TV_USER_AGENT: &'static str = "Crunchyroll/ANDROIDTV/3.70.0_22358 (Android 13.0; en-US; TCL-S5400AF Build/TP1A.220624.014)";
+        pub const ANDROID_TV_USER_AGENT: &'static str = "Crunchyroll/ANDROIDTV/3.71.0_22359 (Android 13.0; en-US; TCL-S5400AF Build/TP1A.220624.014)";
 
         pub const ANDROID_TV_DEFAULT_HEADERS: [(HeaderName, HeaderValue); 4] = [
             (
